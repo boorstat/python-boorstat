7d5b5dec79084346062e3bb31c927781da96321d
Serge Tsygankov <baza@yandex-team.ru>
2016-11-18 01:28:20 +0300

    idiot parsing - func way

--- boorstat/lit/dostoyevsky/idiot/idiot.py
+++ boorstat/lit/dostoyevsky/idiot/idiot.py
@@ -3,28 +3,95 @@
 import tempfile
 import os
 import re
+import roman
+# from itertools import
 
 import requests
 from cached_property import cached_property
 
+TEXT_URL = 'https://github.com/boorstat/boorstat-files/raw/master/lit/dostoevsky/The_Idiot.txt'
 
-class Idiot(object):
-    url = 'https://github.com/boorstat/boorstat.github.io/raw/master/files/dostoevsky/The_Idiot.txt'
 
+def objectify_paragraph(paragraph_text, paragraph_num):
+    pass
+
+
+def objectify_chapter(chapter_text, chapter_num):
+    chapter = {
+        'title': '{}.'.format(roman.toRoman(chapter_num)),
+        'text': chapter_text
+    }
+
+    return chapter
+
+
+def objectify_part(part_text, part_num):
+    part = {
+        'title': 'PART {}'.format(roman.toRoman(part_num)),
+        'text': part_text
+    }
+
+    # chapter_seps = ['{}\\.$'.format(roman.toRoman(i)) for i in range(17, 0, -1)]
+    # print('|'.join(chapter_seps))
+    chapters = re.split(r'^[ IVX]+\.?$', part_text, flags=re.M)
+    chapters = [c.strip() for c in chapters][1:]
+
+    part['chapters'] = [objectify_chapter(c, n + 1) for n, c in enumerate(chapters)]
+
+    return part
+
+
+def objectify_idiot(url=TEXT_URL):
+    text = requests.get(url).text
+
+    idiot = {
+        'title': 'The Idiot',
+        'author': 'Fyodor Dostoyevsky',
+        'text': text}
+
+    part_seps = ['PART {}'.format(roman.toRoman(i)) for i in range(4, 0, -1)]
+    part_seps.append('Copyright')
+
+    parts = re.split('|'.join(part_seps), text)
+    idiot['copyright'] = parts[-1].strip()
+
+    parts = [p.strip() for p in parts[1:]]
+
+    idiot['parts'] = [objectify_part(p, n + 1) for n, p in enumerate(parts)]
+
+    return idiot
+
+
+idiot = objectify_idiot()
+
+
+quit()
+
+
+class ClumsyIdiot(object):
     def __init__(self, use_cached=True):
         self.use_cached = use_cached
 
-    def parts(self):
-        pass
-        # re.split(self.raw.split)
+    def part(self, part_num):
+        return self.raw_parts[1:-1][part_num - 1]
+
+    def chapters(self, part_num):
+        seps = ['{}.'.format(roman.toRoman(i)) for i in range(1, 17)]
+        return [c.strip() for c in re.split('|'.join(seps), self.part(part_num))]
+
+    @cached_property
+    def raw_parts(self):
+        seps = ['PART {}'.format(roman.toRoman(i)) for i in range(1, 5)]
+        seps.append('Copyright')
+        return [p.strip() for p in re.split('|'.join(seps), self.raw)]
 
     @cached_property
     def copyright(self):
-        pass
+        return self.raw_parts[-1]
 
     def download(self):
         with open(self.cached_filename, 'w') as f:
-            f.write(requests.get(self.url).text)
+            f.write(requesxts.get(TEXT_URL).text)
 
     @cached_property
     def json(self):
@@ -40,4 +107,4 @@ class Idiot(object):
 
     @cached_property
     def cached_filename(self):
-        return os.path.join(tempfile.gettempdir(), self.url.split('/')[-2])
+        return os.path.join(tempfile.gettempdir(), TEXT_URL.split('/')[-2])
