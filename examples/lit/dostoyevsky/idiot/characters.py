9e30ca01f211a50461b4f2b97fc3929319f46e1d
Serge Tsygankov <baza@yandex-team.ru>
2016-12-03 00:43:00 +0300

    idiot characters draft list

---
+++ examples/lit/dostoyevsky/idiot/characters.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+
+CHARACTERS = {
+    'Prince Myshkin': ['Lev Nikolayevich', 'Myshkin', 'prince (?!S)'],
+    'Nastasya Philipovna': ['Nastasia Philipovna', 'Barashkova'],
+    'Parfyon Semyonovich Rogozhin': ['Parfyon', 'Rogozhin'],
+    'General Ivan Fyodorovich Yepanchin': ['general', 'Ivan Fyodorovich'],
+    'Elizaveta Prokofyevna': ['Elizaveta', 'Lizaveta', 'Prokofyevna'],
+    'Alexandra Ivanovna': ['Alexandra'],
+    'Adelaida Ivanovna': ['Adelaida'],
+    'Aglaya Ivanovna': ['Aglaya'],
+    'General Ardalión Alexándrovich Ivolgin': ['general', 'Ivolgin', 'Ardalion'],
+    'Nina Alexandrovna': ['Nina'],
+    'Gavrila Ardalionovich': ['Gavrila', 'Ganya', 'Ganechka', 'Ganka'],
+    'Varvara Ardalionovna': ['Varvara'],
+    'Lukyan Timofeevich Lebedev': ['Lukyan', 'Lebedeff'],
+    'Vera Lukyanovna': ['Vera'],
+    'Ippolit Terentyev': ['Ippolit'],
+    'Ivan Petrovich Ptitsyn': ['Ivan Petrovich', 'Ptitsin'],
+    'Evgeny Pavlovich Radomsky': ['Pavlovich', 'Radomsky'],
+    'Prince S.': ['prince S.'],
+    'Afanasy Ivanovich Totsky': ['Afanasy Ivanovich', 'Totsky'],
+    'Ferdyshchenko': ['Ferdyshchenko'],
+    'Keller': ['Keller'],
+    'Antip Burdovsky': ['Antip', 'Burdovsky']
+}
+
+
+
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
